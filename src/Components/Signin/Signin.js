428e4b67bcfc2d28c58913d60cefb44f44f70aba
Caroline Chan <carolinechanccc@gmail.com>
2019-01-26 19:20:49 +0000

    typo in server url

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3001/signin', {
+    fetch('https://protected-mesa-30562.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
